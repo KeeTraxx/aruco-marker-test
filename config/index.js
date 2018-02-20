3633d8857db5aec10f3dec7d8605b15a15ea57b2
Khôi Tran <tran@puzzle.ch>
2018-02-20 17:12:01 +0100

    path for gh-pages

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/aruco-marker-test/',
 
     /**
      * Source Maps
